163dde2c3dff309f338b3346113bcd17015c168f
Celine <celinekurpershoek@gmail.com>
2020-02-10 15:23:10 +0100

    Add licence & rename arguments & move name to top to show in actions tab (#1)
    * Add licence & rename arguments & move name to top to show in actions tab
    
    * fix(entrypoint): remove hash for installation of npm package
    
    * fix(entrypoint): text changes and renamed variable in action.yml
    
    * fix(broken-run): make test succeed by ignoring the broken url + add set -e to make the script fail when something goes wrong
    
    * fix(broken-run): exit with 0
    
    * fix(broken-run): compacter script; test out verbose mode
    
    * fix(broken-run): add quotes and use elif
    
    * fix(broken-run): check if grep returns anything otherwise set to 0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Link checker
+# Broken link check action
 
 This action uses: https://github.com/stevenvachon/broken-link-checker
 
@@ -9,7 +9,7 @@ Create a new file in your repository .github/workflows/action.yml.
 
 Copy-paste the folloing workflow in your action.yml file:
 
-```js
+```yml
 on: [push]
 
 jobs:
@@ -21,18 +21,33 @@ jobs:
       id: link-report
       uses: celinekurpershoek/github-actions-link-checker@master
       with:
-        url: '{Site url to check}' /* (https://..) */
-      follow: false /* optional */
-      ignoreUrls: 'github' /* optional */
+        # Required:
+        url: 'https://...'
+        # optional:
+        honorRobotExclusions: false
+        ignorePatterns: 'github'
     - name: Get the result
       run: echo "${{steps.link-report.outputs.result}}"
 ```
 
-### todo:
+## Optional paramters:
+
+### `honorRobotExclusions`
+Type: `Boolean`
+Default value: `true`
+The script does not scan pages that search engine crawlers would not follow.
+https://github.com/stevenvachon/broken-link-checker#honorrobotexclusions
+
+### `ignorePatterns`
+type: `String`
+Default value: `''`
+A comma separted string of matched urls to ignore. Check documentation about patterns here: https://github.com/stevenvachon/broken-link-checker#excludedkeywords
+
+## todo:
 - [ ] Create issue if broken urls are found
 - [ ] Parse each broken link in report on new line
 
 
-### Test
+## Test
 There is a broken link in this document as a test:
 [A broken link](http://jhgfdsadfghjklkjhgfdsasdfgh.com)
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Fail when any task exits with a non zero error;
+set -e
 
 NC='\033[0m' # No Color
 RED='\033[0;31m'
@@ -6,51 +8,70 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;34m'
 
+# Install the broken-link-checker module globally on the docker instance
 npm i -g broken-link-checker -s
 
 echo -e "$PURPLE=== BROKEN LINK CHECKER ===$NC"
 
-# todo map variables to names
+# @todo map variables to names
 echo -e "Running broken link checker on url: $GREEN $1 $NC"
 
-# IGNORE_PATTERNS=`jq '.ignorePatterns | .[] | .pattern' $CONFIG_FILE`
-echo -e "with settings: $PURPLE skip no-follow links: $YELLOW$2, $PURPLE exclude urls that match: $YELLOW$3 $NC"
+# Create exclude and settings strings based on configuration
+EXCLUDE="" 
+SET_FOLLOW=""
 
-# Create exclude string based on config file
-EXCLUDE=""
+if [ -z "$1" ] || [ "$1" == 'https://github.com/celinekurpershoek/github-actions-link-checker' ]
+then
+    echo -e "$YELLOW Warning: Running test on default url, please provide an url in you action yml.$NC"
+fi
 
-for PATTERN in $3; do
+# Set arguments for blc
+[ "$2" == false ] && SET_FOLLOW="--follow"
+
+for PATTERN in ${3//,/ }; do
     EXCLUDE+="--exclude $PATTERN "
 done
 
-SET_FOLLOW=""
-if [ $2 = false ] 
-then
-    SET_FOLLOW+="--follow"
-fi
+# Echo settings if any are set
+echo -e "Configuration: Honor robot exclusions: $GREEN$2$NC, Exclude urls that match: $GREEN$3$NC \n"
+
+# problem, when there is no broken link there is no output.
+# when run with --follow its fine
+# when run without --follow it gives nothing back.
+
+
 
 # Create command and remove extra quotes
-COMMAND=`echo "blc $1 $EXCLUDE $SET_FOLLOW" | sed 's/"//g'`
+# Put result in variable to be able to iterate on it later
+OUTPUT="$(blc "$1" "$EXCLUDE" $SET_FOLLOW -v | sed 's/"//g')"
 
-# Put result in variable with $()
-OUTPUT=`exec $COMMAND`
+# Count lines of output
+TOTAL_COUNT="$(wc -l <<< "$OUTPUT")"
 
-# Count broken and total links
-BROKEN_COUNT=`grep -o 'BROKEN' <<< $OUTPUT | grep 'BROKEN' -c`
-TOTAL_COUNT=`grep -o '├' <<< $OUTPUT | grep '├' -c`
+if grep -q 'BROKEN' <<< "$OUTPUT" 
+then
+    BROKEN="$(grep -q 'BROKEN' <<< "$OUTPUT")"
+    BROKEN_COUNT="$(wc -l <<< "$BROKEN")"
+else 
+    BROKEN_COUNT=0
+fi
 
-if [ $BROKEN_COUNT -gt 0 ]
+if [ "$BROKEN_COUNT" -gt 0 ] 
 then 
     RESULT="$BROKEN_COUNT broken url(s) found ($TOTAL_COUNT total)" 
-    echo -e "$RED ✗ Failed $RESULT: $NC"
-    #todo put each result on new line:
-    echo `grep -E 'BROKEN' <<< $OUTPUT | awk '{print "- " $2 "\n" }'`
+    echo -e "$RED Failed $RESULT: $NC"
+    # @todo put each result on new line:
+    grep -E 'BROKEN' <<< "$OUTPUT" | awk '{print "[✗] " $2 "\n" }'
+    echo -e "$PURPLE ============================== $NC"
+    echo ::set-output name=result::"$RESULT"
     exit 1
+elif [ "$TOTAL_COUNT" == 0 ]
+then
+    echo -e "Did'nt find any links to check"
 else 
     RESULT="✓ Checked $TOTAL_COUNT link(s), no broken links found!"
     echo -e "$GREEN $RESULT $NC"
+    echo ::set-output name=result::"$RESULT"
+    echo -e "$PURPLE ============================== $NC"
 fi
-
-echo ::set-output name=result::$RESULT
-
-echo -e "$PURPLE ============================== $NC"
+exit 0

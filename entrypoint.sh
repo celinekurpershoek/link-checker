32cf5ce967f0cea8036aea075643b2d6f4a18aaf
Celine Kurpershoek <celinekurpershoek@gmail.com>
2020-02-10 11:53:06 +0100

    fix(broken-run): compacter script; test out verbose mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Fail when any task exits with a non zero error;
 set -e
 
@@ -17,43 +17,36 @@ echo -e "$PURPLE=== BROKEN LINK CHECKER ===$NC"
 echo -e "Running broken link checker on url: $GREEN $1 $NC"
 
 # Create exclude and settings strings based on configuration
-SETTINGS=""
 EXCLUDE=""
 SET_FOLLOW=""
 
-if [[ $3 != '' && $3 == false ]]
+if [ -z $1 ] || [ $1 == 'https://github.com/celinekurpershoek/github-actions-link-checker' ]
 then
-SETTINGS+="Exclude urls:"
+    echo -e "$YELLOW Warning: Running test on default url, please provide an url in you action yml.$NC"
 fi
 
-for PATTERN in $3; do
+# Set arguments for blc
+[ "$2" == false ] && SET_FOLLOW="--follow"
+
+for PATTERN in ${3//,/ }; do
     EXCLUDE+="--exclude $PATTERN "
-    SETTINGS+=" $GREEN$PATTERN$NC "
 done
 
-if [[ $2 != '' && $2 == false ]] 
-then
-    SET_FOLLOW+="--follow"
-    SETTINGS+="Honor robot exclusions: $GREEN$2$NC "
-fi
-
 # Echo settings if any are set
-if [ -n "$SETTINGS" ] 
-then
-echo -e "Configuration: $SETTINGS \n"
-fi
+echo -e "Configuration: Honor robot exclusions: $GREEN$2$NC, Exclude urls that match: $GREEN$3$NC \n"
 
 # Create command and remove extra quotes
-COMMAND=`echo "blc $1 $EXCLUDE $SET_FOLLOW" | sed 's/"//g'`
+COMMAND=`echo "blc -v $1 $EXCLUDE $SET_FOLLOW" | sed 's/"//g'`
 
 # Put result in variable to be able to iterate on it later
+echo $COMMAND
 OUTPUT=`exec $COMMAND`
-
+echo $OUTPUT
 # Count broken and total links
 BROKEN_COUNT=`grep -o 'BROKEN' <<< $OUTPUT | grep 'BROKEN' -c`
 TOTAL_COUNT=`grep -o '├' <<< $OUTPUT | grep '├' -c`
 
-if [ $BROKEN_COUNT -gt 0 ]
+if [ $BROKEN_COUNT -gt 0 ] 
 then 
     RESULT="$BROKEN_COUNT broken url(s) found ($TOTAL_COUNT total)" 
     echo -e "$RED Failed $RESULT: $NC"
@@ -62,6 +55,10 @@ then
     echo -e "$PURPLE ============================== $NC"
     echo ::set-output name=result::$RESULT
     exit 1
+else if [ $TOTAL_COUNT == 0 ]
+then
+    echo -e "Did'nt find any links to check"
+    exit 0
 else 
     RESULT="✓ Checked $TOTAL_COUNT link(s), no broken links found!"
     echo -e "$GREEN $RESULT $NC"

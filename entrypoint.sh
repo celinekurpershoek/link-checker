584b6b04de578ff94cd26aa6f0c807bdba1883a6
Celine Kurpershoek <celinekurpershoek@gmail.com>
2020-09-04 15:09:04 +0200

    feat(recursive): remove quotes for boolean

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,11 +33,14 @@ for PATTERN in ${3//,/ }; do
 done
 
 # Echo settings if any are set
-echo -e "Configuration: Honor robot exclusions: $GREEN$2$NC, Exclude urls that match: $GREEN$3$NC \n"
+echo -e "Configuration: \n 
+Honor robot exclusions: $GREEN$2$NC, \n
+Exclude urls that match: $GREEN$3$NC \n
+Resursive urls: $GREEN$4$NC"
 
 # Create command and remove extra quotes
 # Put result in variable to be able to iterate on it later
-OUTPUT="$(blc "$1" "$EXCLUDE" $SET_FOLLOW "$SET_RECURSIVE" -v | sed 's/"//g')"
+OUTPUT="$(blc "$1" "$EXCLUDE" $SET_FOLLOW $SET_RECURSIVE -v | sed 's/"//g')"
 
 # Count lines of output
 TOTAL_COUNT="$(wc -l <<< "$OUTPUT")"
